9f8431b6fa2fc9f0392ac44f49d64f1d7214a575
Natalia Beckstead <toosmiravilla@gmail.com>
2020-08-07 11:19:11 -0600

    ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,22 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    
+    num_hash = {}
+
+    for i in arrays[0]:
+        num_hash[i] = 0
+
+    for i in range(1, len(arrays)):
+        for num in arrays[i]:
+            if num in num_hash:
+                if num_hash[num] == i - 1:
+                    num_hash[num] = i
+                else:
+                    del num_hash[num]
+
+    result = []
+    for item in num_hash.items():
+        if item[1] == len(arrays) - 1:
+            result.append(item[0])
 
     return result
 
@@ -15,3 +29,4 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
